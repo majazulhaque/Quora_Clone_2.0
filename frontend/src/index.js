56103aafcfdd496694ef7a53284e66c36bb4efa2
Majaz <majazulhaque123@gmail.com>
2023-06-13 18:37:14 +0530

    create firebase and implement provider,store and set into index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 import TimeAgo from "javascript-time-ago";
 
 import en from "javascript-time-ago/locale/en.json";
-// import { Provider } from "react-redux";
-// import store from "./app/store";
-import ru from "javascript-time-ago/locale/ru.json";
+import { Provider } from "react-redux";
+import store from "./app/store";
+// import ru from "javascript-time-ago/locale/ru.json";
 
 TimeAgo.addDefaultLocale(en);
-TimeAgo.addLocale(en);
+// TimeAgo.addLocale(en);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
