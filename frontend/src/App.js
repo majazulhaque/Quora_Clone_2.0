65019afd48e29a997a24ce8c9a6e0ea63cab2ae9
Majaz <majazulhaque123@gmail.com>
2023-06-13 19:24:59 +0530

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
 import Quora from './components/Quora';
+import Login from './components/auth/Login';
+import { selectUser } from './feature/userSlice';
+import {useSelector} from "react-redux";
 
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className="App">
-      <Quora/>
+      {
+        user ? (<Quora/>):(<Login/>)
+      }
     </div>
   );
 }
